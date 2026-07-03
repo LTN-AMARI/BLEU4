0b6b96de1e5c208caa79f46825e6e3f745490169
LTN-AMARI <jc.batimoz@gmail.com>
2026-07-02 23:18:01 -1000

    Initialize Firebase and implement event management

---
+++ data.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, push, set, onValue, remove } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyCOTja98aA-0umXrqm2c2k4frUFn6why1o",
+authDomain: "bleu-4.firebaseapp.com",
+databaseURL: "https://bleu-4-default-rtdb.firebaseio.com",
+projectId: "bleu-4",
+storageBucket: "bleu-4.appspot.com",
+messagingSenderId: "788139266954",
+appId: "1:788139266954:web:c1896f25eb57687846ae73"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.currentUser = JSON.parse(localStorage.getItem("BLEU4_USER"));
+if(!window.currentUser) location.href="index.html";
+
+window.logout=()=>{
+localStorage.clear();
+location.href="index.html";
+};
+
+let events=[];
+window.events=events;
+
+onValue(ref(db,"events"),snap=>{
+events=[];
+snap.forEach(c=>{
+events.push({id:c.key,...c.val()});
+});
+if(window.refreshUI) window.refreshUI();
+});
+
+window.addMission=(title,start,end,concerned)=>{
+push(ref(db,"events"),{
+title,start,end,concerned,
+participants:{},
+indisponibles:{}
+});
+};
+
+window.getEventsByDate=(date)=>{
+return events.filter(e=>e.start<=date && e.end>=date);
+};
+
+window.deleteEvent=(id)=>{
+remove(ref(db,"events/"+id));
+};
+
+window.setParticipation=(id,status)=>{
+const ev=events.find(e=>e.id===id);
+if(!ev) return;
+
+if(!ev[status]) ev[status]={};
+ev[status][window.currentUser.login]=window.currentUser;
+
+set(ref(db,"events/"+id),ev);
+};
